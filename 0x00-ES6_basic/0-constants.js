06cd5671fa67c539e414fd4b49c64ed757e4d090
amuMabirimise <awmabirimise@gmail.com>
2024-03-04 07:36:27 -0800

    const or let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+const taskFirst = () => {
+	var task = 'I prefer const when I can.';
+	return task;
+}
+
+const getLast = () => {
+	return ' is okay';
+}
+
+let taskNext = () => {
+	var combination = 'But sometimes let';
+	combination += getLast();
+
+	return combination;
+}
+
+export { taskFirst, taskNext };
