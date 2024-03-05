73d0515efbbc521ac3dd03bd38b6388f4a59a1fb
amuMabirimise <awmabirimise@gmail.com>
2024-03-05 10:35:28 -0800

    method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,13 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-	const budget = getBudgetObject(income, gdp, capita);
-	const fullBudget = {
-		...budget,
-		getIncomeInDollars: function (income) {
-			return `$${income}`;
-		},
-		getIncomeInEuros: function (income) {
-			return `${income} euros`;
-		},
-	};
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
 
-	return fullBudget;
+  return fullBudget;
 }
+
