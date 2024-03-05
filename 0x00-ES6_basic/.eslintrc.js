645397b0de04d393ea422bd075f76002de9e8e97
amuMabirimise <awmabirimise@gmail.com>
2024-03-05 04:53:11 -0800

    fix semi

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2018
   },
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-  },
+    semi: ['error', 'always']
+  }
 };
-
