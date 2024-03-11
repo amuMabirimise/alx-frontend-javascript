e48005b6810db7357bf6b4b0c6612486fe74f25f
amuMabirimise <awmabirimise@gmail.com>
2024-03-11 10:36:37 -0700

    list objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+class Student {
+  constructor(id, firstName, location) {
+    this.id = id;
+    this.firstName = firstName;
+    this.location = location;
+  }
+}
+
+function getListStudents() {
+  const students = [
+    new Student(1, 'Guillaume', 'San Francisco'),
+    new Student(2, 'James', 'Columbia'),
+    new Student(5, 'Serena', 'San Francisco')
+  ];
+
+  return students;
+}
+
+export default getListStudents;
+
